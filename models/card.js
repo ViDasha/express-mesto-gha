66140430ad7d959c04880abdf97b7c8eef22b7bc
Daria Vinogradova <vinodarya@yandex.ru>
2022-08-06 22:55:29 +0300

    feat: add js files, user&card models

---
+++ models/card.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+const cardSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    minlength: 2,
+    maxlength: 30,
+  },
+  about: {
+    type: String,
+    required: true,
+  },
+  owner: {
+    type: Schema.Types.ObjectId,
+    required: true,
+  },
+  likes: [{
+    type: Schema.Types.ObjectId,
+    default: []
+  }],
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+// создаём модель и экспортируем её
+module.exports = mongoose.model('card', cardSchema);
