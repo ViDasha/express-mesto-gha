002066384aa48521da666ae56926828a659b942d
Daria Vinogradova <vinodarya@yandex.ru>
2022-08-14 17:54:49 +0300

    feat: add routes signin, signup

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const { login, createUser } = require('./controllers/users');
 
 const { PORT = 3000 } = process.env;
 
@@ -10,6 +11,9 @@ const app = express();
 mongoose.connect('mongodb://localhost:27017/mestodb', {
   useNewUrlParser: true,
 });
+
+app.post('/signin', login);
+app.post('/signup', createUser);
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
--- routes/users.js
+++ routes/users.js
@@ -3,14 +3,12 @@ const router = require('express').Router();
 const {
   getUsers,
   getUserById,
-  createUser,
   updateProfile,
   updateAvatar,
 } = require('../controllers/users');
 
 router.get('/:userId', getUserById);
 router.get('/', getUsers);
-router.post('/', createUser);
 router.patch('/me', updateProfile); // обновляет профиль
 router.patch('/me/avatar', updateAvatar); // обновляет аватар
 
