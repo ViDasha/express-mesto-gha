b60ee9c2f8263fd7abc1b0a6119c9ab7b30d91f1
Daria Vinogradova <vinodarya@yandex.ru>
2022-08-14 16:57:13 +0300

    feat: add email, password in user model

--- models/user.js
+++ models/user.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const isEmail = require('validator/lib/isEmail');
 
 const userSchema = new mongoose.Schema({
   name: {
@@ -16,6 +17,21 @@ const userSchema = new mongoose.Schema({
   avatar: {
     type: String,
     required: true,
+  },
+  email: {
+    type: String,
+    require: true,
+    unique: true,
+    validate: [
+      isEmail,
+      {
+        message: 'Неверный формат E-mail',
+      },
+    ],
+  },
+  password: {
+    type: String,
+    require: true,
   },
 }, {
   versionKey: false, // You should be aware of the outcome after set to false
