7a35db83feea7b07fa45c3d0c0a6b2dfa1185056
Daria Vinogradova <vinodarya@yandex.ru>
2022-08-23 23:00:09 +0300

    feat: add validate email

--- models/user.js
+++ models/user.js
@@ -18,6 +18,10 @@ const userSchema = new mongoose.Schema({
   avatar: {
     type: String,
     default: 'https://pictures.s3.yandex.net/resources/jacques-cousteau_1604399756.png',
+    validate: {
+      validator: (v) => /^(https?:\/\/)(www\.)?[a-z\d\D]*/.test(v),
+      message: 'Неверный формат ссылки!',
+    },
   },
   email: {
     type: String,
