29e2abd13f650c0dda593a625ee1e73c69836d40
Daria Vinogradova <vinodarya@yandex.ru>
2022-08-28 22:40:48 +0300

    fix: last test

--- routes/users.js
+++ routes/users.js
@@ -15,7 +15,7 @@ router.get('/me', getUser);
 
 router.get('/:userId', celebrate({
   params: Joi.object().keys({
-    userId: Joi.string().alphanum().required(),
+    userId: Joi.string().alphanum().required().length(24),
   }),
 }), getUserById);
 
